cccf3019203726213d15b0b9409f5a9f62c353c8
oceanshowers3 <73365515+oceanshowers3@users.noreply.github.com>
2021-05-21 15:06:36 -0400

    added comparison donuts, needs edits

--- Website/static/scripts/heartdonut.js
+++ Website/static/scripts/heartdonut.js
@@ -1,25 +1,28 @@
-// Variable set up
-// donut
-const stroke = "http://127.0.0.1:5000/api/v1.0/stroke_1"
+// API Set Up
+var stroke = "http://127.0.0.1:5000/api/v1.0/stroke_2"
 
-// Render original charts
-
-// Default Donut Chart
-var donutLoad = (defaultDonut => {
+// Stroke Donut Chart
+var donutLoadStroke = (donut => {
   d3.json(stroke).then(data => {
 
-    // set default movie title
+    // set stroke value (yes)
     var searchStroke = 1;
 
-    var total = data.heart_disease.length
+    // set heart disease value
+    var searchHeart = 1;
 
-    // filter through data to find title
-    var heartdiseaseYes = data.heart_disease.filter(heart => heart === searchStroke).length  
+    // find length of those that have had strokes
+    var strokeLength = data.filter(count => count.stroke === searchStroke).length
 
+    // filter through data to find those with strokes and with heart disease, find length
+    var strokeYes = data.filter(yes => yes.stroke === searchStroke && yes.heart_disease === searchHeart).length;
+
+    // calculate and set values for those with and without heart disease (w/ stroke)
     var heartData = [
-      {x: "Yes", value: heartdiseaseYes},
-      {x: "No", value: total - heartdiseaseYes}
-    ]
+      {x: "Yes", value: strokeYes},
+      {x: "No", value: strokeLength - strokeYes}
+    ];
+
     // create pie chart, set data
     chart = anychart.pie(heartData);
 
@@ -33,7 +36,7 @@ var donutLoad = (defaultDonut => {
     chart.connectorStroke({color: "#595959", thickness: 2, dash:"2 2"});
 
     // set the container id
-    chart.container("plotDonut");
+    chart.container("plotDonutStroke");
 
     // create and configure a label
     var label = anychart.standalones.label();
@@ -52,5 +55,62 @@ var donutLoad = (defaultDonut => {
   })
 })
 
-// Load default donut chart 
-donutLoad()
+// Load Stroke Donut Chart
+donutLoadStroke()
+
+// No Stroke Donut Chart
+var donutLoadNull = (donut => {
+  d3.json(stroke).then(data => {
+
+    // set stroke value (no)
+    var searchStroke = 0;
+
+    // set heart disease value
+    var searchHeart = 1;
+
+    // find length of those that have not had strokes
+    var strokeLength = data.filter(count => count.stroke === searchStroke).length
+
+    // filter through data to find those without strokes and with heart disease, find length
+    var strokeNo = data.filter(no => no.stroke === searchStroke && no.heart_disease === searchHeart).length;
+
+    // calculate and set values for those with and without heart disease (w/o stroke)
+    var heartData = [
+      {x: "Yes", value: strokeNo},
+      {x: "No", value: strokeLength - strokeNo}
+    ];
+
+    // create pie chart, set data
+    chart = anychart.pie(heartData);
+
+    /* set the inner radius(to turn the pie chart into a doughnut chart)*/
+    chart.innerRadius("50%");
+
+    // set the position of labels
+    chart.labels().position("outside");
+
+    // configure connectors
+    chart.connectorStroke({color: "#595959", thickness: 2, dash:"2 2"});
+
+    // set the container id
+    chart.container("plotDonutNull");
+
+    // create and configure a label
+    var label = anychart.standalones.label();
+    label.text("Heart Disease");
+    label.width("100%");
+    label.height("100%");
+    label.fontColor("#60727b");
+    label.hAlign("center");
+    label.vAlign("middle");
+
+    // set the label as the center content
+    chart.center().content(label);
+
+    // initiate drawing the chart
+    chart.draw();
+  })
+})
+
+// Load No Stroke Donut Chart 
+donutLoadNull()
